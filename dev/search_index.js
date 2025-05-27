079b3cca8787ec8cdb7d0612cace568a29f4fe43
Documenter.jl <documenter@juliadocs.github.io>
2025-05-27 19:27:11 +0000

    build based on 9e45eb7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#BootstrapBill","page":"Home","title":"BootstrapBill","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BootstrapBill.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
